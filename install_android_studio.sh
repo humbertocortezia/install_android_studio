a00e6dc4cd35b41095d0a58407a29b27045acae0
humbertocortezia <36707224+humbertocortezia@users.noreply.github.com>
2024-09-02 11:08:43 -0400

    Atualizando o script para instalar o android studio no linux 02/09/2024

--- install_android_studio.sh
+++ install_android_studio.sh
@@ -1,24 +1,54 @@
 #!/bin/bash
 
-# Baixar o Android Studio do link fornecido e mover para a pasta /tmp
-wget -O /tmp/android-studio.tar.gz "https://encurtador.com.br/svJMQ"
+# Definindo variáveis
+ANDROID_STUDIO_URL="https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2024.1.2.12/android-studio-2024.1.2.12-linux.tar.gz"
+ANDROID_STUDIO_TAR="/tmp/android-studio.tar.gz"
+ANDROID_STUDIO_DIR="/opt/android_studio"
+DESKTOP_DIR=$(xdg-user-dir DESKTOP)
+ICON_URL="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Android_Studio_Icon_3.6.svg/1900px-Android_Studio_Icon_3.6.svg.png"
+ICON_PATH="$ANDROID_STUDIO_DIR/bin/studio.png"
+
+# Verificar se o arquivo já existe
+if [ -f $ANDROID_STUDIO_TAR ]; then
+    echo "O arquivo $ANDROID_STUDIO_TAR já existe. Usando o arquivo existente."
+else
+    # Baixar o Android Studio
+    wget -O $ANDROID_STUDIO_TAR $ANDROID_STUDIO_URL
+    if [ $? -ne 0 ]; then
+        echo "Falha ao baixar o Android Studio."
+        exit 1
+    fi
+fi
 
 # Descompactar o Android Studio
-tar -xvzf /tmp/android-studio.tar.gz -C /tmp/
+tar -xvzf $ANDROID_STUDIO_TAR -C /tmp/
+if [ $? -ne 0 ]; then
+    echo "Falha ao descompactar o Android Studio."
+    exit 1
+fi
 
-# Mover para a pasta /opt
-mkdir -p /opt/android_studio
-mv /tmp/android-studio/* /opt/android_studio/
+# Criar diretório de destino e mover os arquivos
+sudo mkdir -p $ANDROID_STUDIO_DIR
+# Usar rsync para mover e mesclar os arquivos de forma segura
+sudo rsync -a --delete /tmp/android-studio/ $ANDROID_STUDIO_DIR/
+if [ $? -ne 0 ]; then
+    echo "Falha ao mover o Android Studio para /opt."
+    exit 1
+fi
 
-# Fazer o download da imagem e renomeá-la para studio.png
-wget -O /opt/android_studio/bin/studio.png "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Android_Studio_Icon_3.6.svg/1900px-Android_Studio_Icon_3.6.svg.png"
+# Fazer o download do ícone
+sudo wget -O $ICON_PATH $ICON_URL
+if [ $? -ne 0 ]; then
+    echo "Falha ao baixar o ícone do Android Studio."
+    exit 1
+fi
 
 # Definir as variáveis de ambiente
-export ANDROID_HOME=/opt/android_studio
+export ANDROID_HOME=$ANDROID_STUDIO_DIR
 export PATH=$PATH:$ANDROID_HOME/bin
 
-# Criar o atalho na área de trabalho
-cat > ~/Área\ de\ Trabalho/Android\ Studio.desktop <<EOL
+# Criar o atalho na área de trabalho detectada
+cat > "$DESKTOP_DIR/Android_Studio.desktop" <<EOL
 [Desktop Entry]
 Version=1.0
 Type=Application
@@ -31,9 +61,9 @@ StartupNotify=true
 EOL
 
 # Dar permissão de execução ao atalho
-chmod +x ~/Área\ de\ Trabalho/Android\ Studio.desktop
+chmod +x "$DESKTOP_DIR/Android_Studio.desktop"
 
 # Excluir o arquivo compactado após a instalação
-rm /tmp/android-studio.tar.gz
+rm $ANDROID_STUDIO_TAR
 
 echo "Android Studio foi instalado com sucesso!"
